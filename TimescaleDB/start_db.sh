5edab192ad23dc3980ba5bc9d03623395bdeb3e9
Juan Andrés Avelino Ospina <javelino2311@gmail.com>
2022-12-02 01:35:03 -0500

    Updated files on Data layer

--- TimescaleDB/start_db.sh
+++ TimescaleDB/start_db.sh
@@ -1,1 +1,5 @@
-docker run --name timescaledb -it -p 5432:5432 -v "//$PWD/data:/home/postgres/pgdata/data" -e POSTGRES_PASSWORD=IOT2022! -e POSTGRES_USER=labmonitoring -e POSTGRES_DB=solarlabmonitoring timescale/timescaledb:latest-pg14
+# For Windows
+docker run --name timescaledb -dit -p 5432:5432 -v "//$PWD/data:/var/postgres/pgdata/data" -e POSTGRES_PASSWORD=IOT2022! -e POSTGRES_USER=labmonitoring -e POSTGRES_DB=solarlabmonitoring timescale/timescaledb:latest-pg14
+
+# For Linux
+docker run --name timescaledb -dit -p 5432:5432 -v $(pwd)/data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=IOT2022! -e POSTGRES_USER=labmonitoring -e POSTGRES_DB=solarlabmonitoring timescale/timescaledb:latest-pg14
