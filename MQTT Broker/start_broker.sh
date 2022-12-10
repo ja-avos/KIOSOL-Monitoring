f6218030dd9c2c871b2872b460939fdcefd5803c
Juan Andrés Avelino Ospina <javelino2311@gmail.com>
2022-12-10 10:19:19 -0500

    Updated MQTT conf

--- MQTT Broker/start_broker.sh
+++ MQTT Broker/start_broker.sh
@@ -1,1 +1,5 @@
-docker run --name mqtt -it -p 1883:1883 -p 9001:9001 -v "//$PWD/mosquitto.conf:/mosquitto/config/mosquitto.conf" eclipse-mosquitto
+# Windows
+#docker run --name mqtt -it -p 1883:1883 -p 9001:9001 -v "//$PWD:/mosquitto/config" eclipse-mosquitto
+
+# Linux
+docker run --name mqtt -it -p 1883:1883 -p 9001:9001 -v $(pwd):/mosquitto/config eclipse-mosquitto
